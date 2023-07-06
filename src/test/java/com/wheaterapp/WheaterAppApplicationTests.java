751e6d1ce35452d8407b8eea8e7c0eb14f71d086
Konrad Patyk <kooko96@gmail.com>
2023-07-06 18:19:21 +0200

    Initial commit

---
+++ src/test/java/com/wheaterapp/WheaterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wheaterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheaterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
